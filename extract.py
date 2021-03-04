42f203e5e198cbc3e54ea89ccd983e7e24c17516
7000WORDS <emekadrake707@gmail.com>
2021-03-04 23:54:06 +0000

    succes ig

---
+++ extract.py
@@ -0,0 +1,13 @@
+u_name = input("enter your username: ")
+while u_name != username:
+    print("try again")
+    u_name = input()
+if u_name == username:
+    print("you got it right.")
+    
+p_word = input("enter your password: ")
+while p_word != password:
+    print("try again: ")
+    p_word = input()
+if p_word == password:
+    print("you got it right.")
--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 from tkinter import *
-tk = Tk()
-tk.title("Test")
-tk.mainloop()
+import tkinter as tk
+from tkinter import messagebox
+
 
 username = input("set your username: ")
 password = input("set a password: ")
 
-u_name = input("enter your username: ")
-while u_name != username:
-    print("try again")
-    u_name = input()
-if u_name == username:
-    print("you got it right.")
+def Ok():
 
-p_word = input("enter your password: ")
-while p_word != password:
-    print("try again: ")
-    p_word = input()
-if p_word == password:
-    print("you got it right.")
-    
+    uname = e1.get()
+    pword = e2.get()
+
+    if(uname == "" and pword == ""):
+            messagebox.showinfo("", "Blank Not allowed")
+
+    elif(uname == username and pword== password):
+
+            messagebox.showinfo("", "Login Success")
+            root.destroy()
+
+    else:
+            messagebox.showinfo("", "Incorrent Username and Password")
+
+
+root = Tk()
+root.title("Login")
+root.geometry("300x200")
+global e1
+global e2
+
+Label(root, text="UserName").place(x=10, y=10)
+Label(root, text="Password").place(x=10, y=40)
+
+e1 = Entry(root)
+e1.place(x=140, y=10)
+
+e2 = Entry(root)
+e2.place(x=140, y=40)
+e2.config(show="*")
+
+
+Button(root, text="Login", command=Ok, height=3, width=13).place(x=10, y=100)
+
+root.mainloop()
